796eb8b3aba677111178e6446816ab21ec29f8c6
tkdduq123 <tkdduq0725@gmail.com>
2025-08-05 21:56:17 +0900

    Add files via upload

---
+++ 소스0805.cpp
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("æ»≥Á«œººø‰\n");
+//	return 0;
+//}
+//#include <stdio.h>
+
+//int main()
+//{
+//	printf("æ»≥Á«œººø‰");
+//	return 0;
+//}
+#include <stdio.h>
+
+int main()
+{
+	printf("æ»≥Á«œººø‰\næ»≥Á«œººø‰\")
+}
