e1b718622faacc362ac9b546c15591ea078b598b
tkdduq123 <tkdduq0725@gmail.com>
2025-08-07 21:52:12 +0900

    Add files via upload

---
+++ 소스0807.cpp
@@ -0,0 +1,112 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요\n안녕하세요\n안녕하세요\n");
+//	return 0;
+//}
+//#include <stdio.h>
+
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	printf("안녕하세요");
+//	printf("안녕하세요\n");
+//	return 0;
+//}
+//#include <stdio.h>
+
+//int main()
+//{
+//	printf("사과 1개\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//{
+//	int apple = 1;
+//	printf("사과 %개\n", apple);
+//	return 0;
+//}
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("키보드 3개\n");
+//	printf("마우스 2개\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int keyboard = 3;
+//	int mouse = 2;
+//	printf("마우스: %d개\n", mouse);
+//	printf("키보드: % d개\n", keyboard);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int coin1 = 500;
+//	int coin2 = 1000;
+//	printf("%d+%d=%\n",
+//		coin1, coin2, coin1 + coin2);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char A;
+//	A = 'A';
+//	printf("A=%c, A=%d\n", A, A);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double pi;
+//	pi = 3.14;
+//	float e = 2.718;
+//	printf("pi=%.2lf, r=%.3f..\n", pi, e);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int radius = 5;
+//	double pi = 3.14;
+//	
+//	double area_of_acircle = radius * radius * pi;
+//	printf("원의넓이: %.2lf", area_of_acircle);
+//
+//	double girth = radius * 2 * pi;
+//	printf("    원의둘레: %lf", girth);
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int bottom_side = 3;
+//	int upper_edge = 5;
+//	int height = 7;
+//	int trapezoid_area = (bottom_side + upper_edge) * height/2;
+//	printf("사다리꼴의넓이: %d", trapezoid_area);
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int k;
+//	printf("원하는 숫자를 입력 : ");
+//	scanf("%d", &k); //vs: scanf_s
+//	printf("입력한 숫자는 : %d\n", k );
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	int y, x;
+	printf("더하고")
+}
