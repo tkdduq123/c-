b394d14f16f44d64870394d75a2b74c0c919d167
tkdduq123 <tkdduq0725@gmail.com>
2025-08-14 19:12:52 +0900

    Add files via upload

--- 소스0814.cpp
+++ 소스0814.cpp
@@ -1,45 +1,11 @@
-#define _CRT_SWARNINGS
-#include <stdio.h>
-void main()
-{
-	int a, b;
-	int result;
-	char k;
-
-	printf("첫 번쨰 계산할 값==>");
-	scanf("%d", &a);
-	printf("+-*/%==>");
-	scanf("%c", &k);
-	printf("두 번째 계산할값==>");
-	scanf("%d", &b);
-
-	if (k == '+') {
-		result = a + b;
-		printf("%d+%d=%d\n", a, b, result);
-
-	}
-	if (k == '-') {
-		result = a - b;
-		printf("%d-%d=%d\n", a, b, result);
-
-	}
-	if (k == '*') {
-		result = a * b;
-		printf("%d*%d=%d\n", a, b, result);
-	}
-	if (k == '/') {
-		if (b != 0) {
-			result = a / b;
-			printf("%d/%d=%d\n", a, b, result);
-		}else
-			printf("0으로 나누면 안됩니다.\n");
-	}
-	if (k == '%') {
-		if (b != 0) {
-			result = a % b;
-			printf("%d%%%d=%d\n", a, b, result);
-		}
-		else
-			printf("0으로 나누면 나머지값이 안됩니다\n");
-	}
-}
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("\n줄 바꿈\n연습 \n");
+//	printf("\t줄 바꿈\t연습 \n");
+//	printf("이것을\r덮어씁시다 \n");
+//	printf("\a\a\a삐소리 3번 \n");
+//	printf("글자가 \"강조\"되는 효과 \n");
+//	printf("\\\\\\ 역슬래시 세 개 출력 \n");
+//}
