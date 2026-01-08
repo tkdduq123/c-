a6a348bad7705e22e2565dbd2f88b40df06f0cb7
tkdduq123 <tkdduq0725@gmail.com>
2026-01-08 16:52:48 +0900

    Add files via upload

---
+++ 260108.cpp
@@ -0,0 +1,137 @@
+//#include <stdio.h>
+//int main()
+//{
+//	char a;
+//	printf("문자를 입력하세요:");
+//	scanf("%c", &a);
+//	if (65 <= a && a <= 'Z')
+//	{
+//		printf("대문자 입니다.");
+//	}
+//	else if ('a' <= a && a <= 'z')
+//	{
+//		printf("소문자 입니다.");
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number, answer;
+//	scanf("%d %d", &number, &answer);
+//	if (number > answer) {
+//		printf("숫자1이 숫자2보다 큽니다.\n");
+//	}
+//	else /* number<=answer 인 경우 else 실행 */
+//	{
+//		if (number < answer)
+//		{
+//			printf("숫자2가 숫자1보다 큽니다.\n");
+//		}
+//		else
+//		{
+//			printf("숫자1과 숫자2가 같습니다.\n");
+//		}
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int number, answer;
+//	scanf("%d %d", &number, &answer);
+//	if (number > answer) {
+//		printf("숫자1이 숫자2보다 큽니다.\n");
+//	}
+//	else if (number < answer)
+//	{
+//		printf("숫자2가 숫자1보다 큽니다.\n");
+//	}
+//	else
+//	{
+//		printf("숫자1과 숫자2가 같습니다.\n");
+//	}
+//	return 0;
+// }
+//#include <stdio.h>
+//int main()
+//{
+//	int score = 75;
+//	if (score > 90) {
+//		printf("성적은 A등급입니다.\n");
+//	}
+//	else if (score > 80) {
+//		printf("성적은 B등급입니다.\n");
+//	}
+//	else if (score > 70) {
+//		printf("성적은 C등급입니다.\n");
+//	}
+//	else if (score > 60) {
+//		printf("성적은 D등급입니다.\n");
+//	}
+//	else  {
+//		printf("성적은 F등급입니다.\n");
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int number = -11;
+//	if (number > 0) {
+//		printf("number은 양수 입니다.\n");
+//	}
+//	else if (number < 0) {
+//		printf("number은 음수입니다.\n");
+//	}
+//	else {
+//		printf("number은 0입니다.\n");
+//	}
+//	if (number % 2 == 0) {
+//		printf("number은 짝수 입니다.\n");
+//	}
+//	else {
+//		printf("number은 홀수 입니다.\n");
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char a;
+//	scanf("%c", &a);
+//	if ('w' == a) {
+//		printf("윗 방향키를 눌렀습니다.\n");
+//
+//	}
+//	else if ('a' == a) {
+//		printf("우 방향키를 눌렀습니다.\n");
+//
+//	}
+//	else if ('d' == a) {
+//		printf("좌 방향키를 눌렀습니다.\n");
+//
+//	}
+//	else if ('s' == a) {
+//		printf("아랫 방향키를 눌렀습니다.\n");
+//
+//	}
+// return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int gauge;
+//	scanf("%d", &gauge);
+//	if (gauge >= 45 && gauge <= 55) {
+//		printf("perfect");
+//	}
+//	else if (gauge >= 35 && gauge <= 65) {
+//		printf("excellent");
+//	}
+//	else if(gauge>=0&& gauge<=100){
+//		printf("good");
+//	}
+//	return 0;
+//}
