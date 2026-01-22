39c77e6c888cac4b1dbfd13cab795a1e4a3eec66
tkdduq123 <tkdduq0725@gmail.com>
2026-01-22 16:53:44 +0900

    Add files via upload

---
+++ 260122.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	while (0)
+	{
+		printf("조건이 참일 경우 반복출력\n");
+	}
+	return 0;
+}
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	while (i < 100)
+//	{
+//		printf("%3d(%2x)",i,i);
+//		if (i >= 70 && i < 80) { i++; continue; }
+//		if (i % 10 == 9)printf("\n");
+//		if (i == 93)break;
+//		i++;
+//	}
+//}
+
+
+
+
+
+
